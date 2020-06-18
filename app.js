c535d7fca1f9d5e625174b863a2fcf94106e679a
spike123-cmd <66212796+spike123-cmd@users.noreply.github.com>
2020-06-18 15:12:47 -0300

    Aula - o que é Express

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const expres = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('<h1>Minha Lista de Tarefas</h1>');
+})
+
+app.listen(3000, () =>{
+    console.log('Servidor foi iniciado');
+}) 
